101d03f8c75bd266062857eaf469fb9612b6acab
Massimiliano Ziccardi <mziccard@redhat.com>
2016-05-03 17:47:49 +0100

    RHMAP-6015 Implemented runner that performs preparation steps for data export

---
+++ test/stubs/mongo/mongoMocks.js
@@ -0,0 +1,44 @@
+var _ = require('underscore');
+
+function createMongoMocks(collectionObjects) {
+
+  var collections = {};
+
+  // Creating collections
+  _.each(collectionObjects, function(collectionObject, index, context) {
+      collections[collectionObject.name] = {
+        stats: function() {
+          return {
+            size: collectionObject.size
+          }
+        }
+      };
+  });
+
+  var mongoClientMock = {
+    dbMock: {
+      mockedCollections: collections,
+      collection: function(name) {
+        return this.mockedCollections[name];
+      },
+      collectionNames: function(cb) {
+        var keys = Object.keys(this.mockedCollections);
+        cb(null, keys);
+      },
+      close: function(cb) {
+        cb();
+      }
+    },
+    connect: function(mongoUrl, cb) {
+      cb(null, this.dbMock);
+    }
+  };
+
+  var mongoMock = {
+    MongoClient: mongoClientMock
+  };
+
+  return mongoMock;
+}
+
+module.exports=createMongoMocks;
