99a8345f1de78c85766eac92ee222d4040b7a9dc
Stephen Coady <scoady@redhat.com>
2018-02-12 18:38:52 +0000

    fix

--- test/unit/middleware/test_mbaas_app.js
+++ test/unit/middleware/test_mbaas_app.js
@@ -36,48 +36,6 @@ module.exports.test_create_app_db_not_for_all_app_types = function (done){
 };
 
 
-module.exports.test_create_app_db_allowed_app_types = function (done){
-  var mocks = {
-    '../util/mongo.js':{
-      'createDb':function (config, user, pass, name,callback){
-         callback();
-      }
-    },
-    'fh-mbaas-middleware':{
-      "config":function (){
-        return{
-          "mongo":{
-            "host":"test",
-            "port":"port"
-          }
-        }
-      }
-    }
-  };
-  var mbaasApp = proxyquire(undertest,mocks);
-  var req = {
-    "appMbaasModel":{
-      "name":"testapp",
-      "type":"openshift3",
-      "save": function (cb){
-        return cb();
-      },
-      "markModified": function (){}
-    }
-  };
-  var res = {};
-  mbaasApp.createDbForAppTypes(["openshift3"])(req,res,function next(err,ok){
-    assert.ok(!err, "did not expect an error to be returned");
-    assert.ok(ok,"expected a dbconfig to be returned");
-    assert.ok(ok.host === "test","expected a host");
-    assert.ok(ok.port === "port","expected a port");
-    assert.ok(ok.hasOwnProperty("name"),"expected a name property");
-    assert.ok(ok.hasOwnProperty("user"),"expected a user property");
-    assert.ok(ok.hasOwnProperty("pass"),"expected a pass property");
-    done();
-  });
-};
-
 module.exports.test_create_does_not_create_when_dbConf_present = function (done){
   var createCalled = false;
   var mocks = {
@@ -260,4 +218,3 @@ module.exports.test_create_app_userdb_allowed_app_types = function (done){
     done();
   });
 };
-
